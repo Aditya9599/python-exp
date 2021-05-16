37dbbeb95fa4e5477894c4edc5272dff7557fa20
Aditya <82726630+Aditya9599@users.noreply.github.com>
2021-05-16 16:58:15 -0400

    I changed the format of the code

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
 import datetime
 import math
+
+
 def date():
     x = datetime.datetime.now()
     return x
+
 
 def squareRoot(one):
     a = math.sqrt(one)
     return a
 
+
 def threeDates():
     y = datetime.datetime.now()
-    return(y.strftime("%x"), y.strftime("%c"), y.strftime("%Y"))
+    return y.strftime("%x"), y.strftime("%c"), y.strftime("%Y")
+
 
 def addition(numberOne, numberTwo):
-    return (numberOne + numberTwo)
+    return numberOne + numberTwo
+
 
 def subtraction(numberOne, numberTwo):
-    return (numberOne - numberTwo)
+    return numberOne - numberTwo
+
 
 def multiplication(numberOne, numberTwo):
-    return (numberOne * numberTwo)
+    return numberOne * numberTwo
+
 
 def division(numberOne, numberTwo):
-    return (numberOne / numberTwo)
+    return numberOne / numberTwo
