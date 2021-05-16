d8bfb6d58e9ca1831df6c71940dc99d2b07a250e
Aditya <82726630+Aditya9599@users.noreply.github.com>
2021-05-16 16:43:37 -0400

    I made functions that add, subtract, multiply, and divide numbers

--- main.py
+++ main.py
@@ -11,3 +11,15 @@ def squareRoot(one):
 def threeDates():
     y = datetime.datetime.now()
     return(y.strftime("%x"), y.strftime("%c"), y.strftime("%Y"))
+
+def addition(numberOne, numberTwo):
+    return (numberOne + numberTwo)
+
+def subtraction(numberOne, numberTwo):
+    return (numberOne - numberTwo)
+
+def multiplication(numberOne, numberTwo):
+    return (numberOne * numberTwo)
+
+def division(numberOne, numberTwo):
+    return (numberOne / numberTwo)
